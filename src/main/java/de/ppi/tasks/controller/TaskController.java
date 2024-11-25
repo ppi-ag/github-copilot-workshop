507b16328b2055a6796172acc281527701a09916
Helge Kraas <helge.kraas@ppi.de>
2024-11-25 13:33:47 +0100

    Added basis classes

---
+++ src/main/java/de/ppi/tasks/controller/TaskController.java
@@ -0,0 +1,23 @@
+package de.ppi.tasks.controller;
+
+
+import de.ppi.tasks.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private de.ppi.tasks.repository.TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
